d6221415baa69eb4d15799005263902dda699905
Adan <51211285+Adan011@users.noreply.github.com>
2019-09-18 17:54:05 -0500

    mvp

---
+++ friends/src/components/Login.js
@@ -0,0 +1,46 @@
+import React, {useState} from "react";
+import {axiosWithAuth} from "../utils/axiosWithAuth";
+
+
+const Login = props => {
+    const [login, setLogin] = useState({username: "", password: ""})
+
+    const handleChange = e => {
+        setLogin({...login, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setLogin({username: "", password: ""})
+        
+        axiosWithAuth().post("./login", login)
+        .then(res => {
+            localStorage.setItem("token", res.data.payload)
+            props.history.push("/friends")
+        })
+        .catch(err => console.log(err))
+        setLogin({username: "", password: ""})
+    }
+
+    return(
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="username"
+                    value={login.username}
+                    onChange={handleChange}
+                    />
+                <input
+                    type="text"
+                    name="password"
+                    value={login.password}
+                    onChange={handleChange}
+                    />
+                <button type="submit">Log in</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login;
